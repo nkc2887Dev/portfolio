a8d49148f1ea036a83b510776d206c3572bb1e2b
nkc2887dev <nkc2887dev@gmail.com>
2025-03-20 23:14:25 +0530

    portfolio

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: [
+      "fastly.picsum.photos",
+      "raw.githubusercontent.com",
+      "mozilla.github.io",
+      "morth.nic.in",
+      "cors-anywhere.herokuapp.com",
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+
+    return config;
+  },
+  // output: "export",
+};
+
+export default nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      boxShadow: {
+        "3xl":
+          "rgba(145, 158, 171, 0.2) 0px 0px 2px 0px, rgba(145, 158, 171, 0.12) 0px 12px 24px -4px;",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
